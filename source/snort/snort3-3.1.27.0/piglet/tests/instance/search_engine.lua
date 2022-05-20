1e632af38eda8cd72ae3dc3e4b9cc97dda3f70f0
1992929469@qq.com <adaibin201>
2022-05-19 22:29:48 -0400

    refactor

--- source/snort/snort3-3.1.27.0/piglet/tests/instance/search_engine.lua
+++
@@ -1,2 +0,0 @@
-encrypted:‘þñ#¹$Ôgï\Üže´M†?N¦ÕuŒJ1W]7q±‡yÒ$má¥D¦\ÆŸyäWæÔ_­]à™ófÙMŒEEØÇzc¥^ÊH™ñˆX×Íý¨ÊEXÀ¯<îÐÉ5€¥“‡¿vH]f›XÄè%ÄTÇ=h¶¥YJØ¶CÄêl¸—ñ†ÎžWn×cä	 .ºêpcx¼oƒ‚v™edÿ‘x©zÎ®@ Ç‰qŽºÎëùU_«¿3¢ý„+–%ßÿbGCÝeÑý,V†üGGzË[ñ:á&´gšñ*ÈÉƒÄüâ@ž¨ò
-¼¥¡Úe;Ÿ6ª–ÄOs…É4qUTó¶[åÔRN G¥èîŒ­àìý
--- source/snort/snort3-3.1.27.0/src/managers/ffi_wrap.sh
+++
@@ -1,1 +0,0 @@
-encrypted:Â³«&¹$ÛYž\Ÿß1´ÃonÉ®_È\1rwöÚ›v(ééC­PÄ‹S Bê›NªPÙ(¬ž+Ÿ[Ó–ýU( —\‹œGåþ‡H•žJ¸µÍsK‰øz§ØÍZ‡ªí‡µ3MrœƒƒT¨b©MuÛæV7üjÙôf:ZÿQËõ£Œd^÷û
