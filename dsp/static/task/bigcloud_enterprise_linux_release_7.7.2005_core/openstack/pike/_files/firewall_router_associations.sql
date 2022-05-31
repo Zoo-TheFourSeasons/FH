62bc7273ed785e3c3111c9a84f7dc3d439742775
ZooAtmosphereGroup <199292929469@qq.com>
2022-05-31 16:11:36 +0800

    refactor

--- dsp/static/task/bigcloud_enterprise_linux_release_7.7.2005_core/openstack/pike/_files/firewall_router_associations.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `firewall_router_associations` (
-  `fw_id` varchar(36) NOT NULL,
-  `router_id` varchar(36) NOT NULL,
-  PRIMARY KEY (`fw_id`,`router_id`),
-  KEY `router_id` (`router_id`),
-  CONSTRAINT `firewall_router_associations_ibfk_1` FOREIGN KEY (`fw_id`) REFERENCES `firewalls` (`id`) ON DELETE CASCADE,
-  CONSTRAINT `firewall_router_associations_ibfk_2` FOREIGN KEY (`router_id`) REFERENCES `routers` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
